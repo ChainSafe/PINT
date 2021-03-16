c0b1f4f6eabf3504c4de3af9e41baeb69a027f51
Willem Olding <willemolding@gmail.com>
2021-03-17 10:56:44 +1100

    apply clippy fix

--- node/src/command.rs
+++ node/src/command.rs
@@ -103,7 +103,7 @@ impl SubstrateCli for RelayChainCli {
     }
 
     fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
-        polkadot_cli::Cli::from_iter([RelayChainCli::executable_name().to_string()].iter())
+        polkadot_cli::Cli::from_iter([RelayChainCli::executable_name()].iter())
             .load_spec(id)
     }
 
@@ -251,7 +251,7 @@ pub fn run() -> Result<()> {
                 let polkadot_cli = RelayChainCli::new(
                     config.base_path.as_ref().map(|x| x.path().join("polkadot")),
                     relay_chain_id,
-                    [RelayChainCli::executable_name().to_string()]
+                    [RelayChainCli::executable_name()]
                         .iter()
                         .chain(cli.relaychain_args.iter()),
                 );
