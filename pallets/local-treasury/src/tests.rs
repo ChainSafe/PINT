3a3322331e99879f0de4ceea40c087fb33d13244
Willem Olding <willemolding@gmail.com>
2021-03-18 16:09:48 +1100

    refactor tests

--- pallets/local-treasury/src/tests.rs
+++ pallets/local-treasury/src/tests.rs
@@ -4,38 +4,54 @@ use sp_runtime::traits::BadOrigin;
 
 const ASHLEY: AccountId = 0;
 
+fn assert_balances(balances: Vec<(AccountId, Balance)>) {
+	for (account, balance) in balances {
+		assert_eq!(Balances::free_balance(account), balance)
+	}
+}
+
 #[test]
 fn unprivileged_account_can_deposit() {
     const INITIAL_BALANCE: Balance = 100;
-    const SEND: Balance = 10;
+    const AMOUNT: Balance = 10;
 
-    new_test_ext(vec![(ASHLEY, INITIAL_BALANCE)]).execute_with(|| {
-        assert_eq!(Balances::free_balance(ASHLEY), INITIAL_BALANCE);
+    let initial_balances: Vec<(u64, u64)> = vec![
+        (local_treasury_account_id(), 0),
+        (ASHLEY, INITIAL_BALANCE),
+    ];
+
+    let final_balances: Vec<(u64, u64)> = vec![
+        (local_treasury_account_id(), AMOUNT),
+        (ASHLEY, INITIAL_BALANCE - AMOUNT),
+	];
+
+    new_test_ext(initial_balances).execute_with(|| {
         assert_ok!(Balances::transfer(
             Origin::signed(ASHLEY),
             local_treasury_account_id(),
-            SEND
+            AMOUNT
         ));
-        assert_eq!(Balances::free_balance(ASHLEY), INITIAL_BALANCE - SEND);
-        assert_eq!(Balances::free_balance(local_treasury_account_id()), SEND);
+        assert_balances(final_balances);
     });
 }
 
 #[test]
 fn unprivileged_account_cannot_withdraw() {
     const INITIAL_BALANCE: Balance = 100;
-    const AMOUNT: Balance = 1;
+    const AMOUNT: Balance = 10;
 
-    new_test_ext(vec![
-        (local_treasury_account_id(), INITIAL_BALANCE),
-        (ASHLEY, 0),
-    ])
+    let initial_balances: Vec<(u64, u64)> = vec![
+        (local_treasury_account_id(), 0),
+        (ASHLEY, INITIAL_BALANCE),
+    ];
+
+    new_test_ext(initial_balances.clone())
     .execute_with(|| {
         assert_noop!(
             LocalTreasury::withdraw(Origin::signed(ASHLEY), AMOUNT, ASHLEY),
             BadOrigin
         );
-        assert_eq!(Balances::free_balance(ASHLEY), 0);
+        assert_balances(initial_balances);
     });
 }
 
@@ -44,16 +60,23 @@ fn admin_account_can_withdraw() {
     const INITIAL_BALANCE: Balance = 100;
     const AMOUNT: Balance = 1;
 
-    new_test_ext(vec![
+    let initial_balances: Vec<(u64, u64)> = vec![
         (local_treasury_account_id(), INITIAL_BALANCE),
         (ADMIN_ACCOUNT_ID, 0),
-    ])
+    ];
+
+    let final_balances: Vec<(u64, u64)> = vec![
+        (local_treasury_account_id(), INITIAL_BALANCE - AMOUNT),
+        (ADMIN_ACCOUNT_ID, AMOUNT),
+	];
+
+    new_test_ext(initial_balances)
     .execute_with(|| {
         assert_ok!(LocalTreasury::withdraw(
             Origin::signed(ADMIN_ACCOUNT_ID),
             AMOUNT,
             ADMIN_ACCOUNT_ID
         ));
-        assert_eq!(Balances::free_balance(ADMIN_ACCOUNT_ID), AMOUNT);
+        assert_balances(final_balances);
     });
 }
