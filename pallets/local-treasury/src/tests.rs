1adb22b15fe7a743c6ec0ef670ef96d90630a53f
Willem Olding <willemolding@gmail.com>
2021-03-23 12:31:49 +1100

    chore:pallets/local-treasiry: adds spdx headers to local-treasury

--- pallets/local-treasury/src/tests.rs
+++ pallets/local-treasury/src/tests.rs
@@ -1,3 +1,6 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
 use crate::mock::*;
 use frame_support::{assert_noop, assert_ok};
 use pallet_balances::Error as BalancesError;
