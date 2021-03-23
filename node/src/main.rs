70e5dda3942079d4574f1ca496cec446c002a92d
Willem Olding <willemolding@gmail.com>
2021-03-23 12:23:39 +1100

    Willem/add licence lgpl3 (#15)
    * chore:global: Add lgpl-3 licence file
    
    * chore:global: Update readme with licence badge
    
    * chore:global: Update cargo licences
    
    * add spdx header to all .rs files
    
    * Delete .licenserc.yaml

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,3 +1,6 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #![warn(unused_extern_crates)]
 
 mod chain_spec;
--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -1,3 +1,6 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
 
