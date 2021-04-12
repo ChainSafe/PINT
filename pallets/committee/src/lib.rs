cdbcd9b59281a873f5ccb641023e87ae76fcd604
Willem Olding <willemolding@gmail.com>
2021-04-13 07:01:39 +1000

    Willem/add committee pallet voting (#17)
    * feat:pallets-committee: adds skeleton and mplements params and store for committee
    
    * feat:pallets/committee: Adds propose extrinsic and tests
    
    * Apply suggestions from code review
    
    chore:pallets/committee: Updates license in cargo.toml and readme
    
    * Apply suggestions from code review
    
    chore:pallets/committee: Remove old comment
    
    * feat:pallets/committee: Adds members storage and functions to mutate, adds vote extrinsic
    
    * feat:pallets/committee: Adds logic to remove active votes if membership set changes
    
    * feat:pallets/committee: Adds logic for calculating when voting periods should end
    
    * feat:pallets/committee: Adds check when voting that vote period is open
    
    * feat:pallets/committee: Add upkeep that ensures active proposals vec is correct
    
    * chore:pallets/committee: Fixes docs
    
    * Apply suggestions from code review
    
    Co-authored-by: David Ansermino <14164624+ansermino@users.noreply.github.com>
    
    * test:pallets/committee: Adds test for upkeep
    
    * test:pallets/committee: Adds test for voting related calls
    
    * chore:pallets/committee: Apply fmt
    
    * chore:pallets/committee: Adds escapes for event docstrings
    
    * chore:pallets/committee: Adds doc comments for non-trivial functions
    
    * chore:pallets/committee: Use turbofish throughout
    
    * chore:pallets/committee: cargo fmt
    
    * chore:pallet/committee: Tidy up last turbofish
    
    Co-authored-by: David Ansermino <14164624+ansermino@users.noreply.github.com>

--- pallets/committee/src/lib.rs
+++ pallets/committee/src/lib.rs
@@ -11,15 +11,19 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+mod utils;
+
 #[frame_support::pallet]
 // requires unused_unit exception as the #[pallet::event] proc macro generates code that violates this lint
-// requires boxed_local exception as extrincis must accept boxed calls but clippy only sees the local function
+// requires boxed_local exception as extrinsics must accept boxed calls but clippy only sees the local function
 #[allow(clippy::unused_unit, clippy::boxed_local)]
 pub mod pallet {
+    use crate::utils;
     use frame_support::{
         dispatch::{Codec, DispatchResultWithPostInfo},
         pallet_prelude::*,
         sp_runtime::traits::Dispatchable,
+        traits::{ChangeMembers, InitializeMembers},
         weights::{GetDispatchInfo, PostDispatchInfo},
     };
     use frame_system::pallet_prelude::*;
@@ -84,13 +88,58 @@ pub mod pallet {
 
     #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, Default)]
     /// Info for keeping track of a motion being voted on.
-    pub struct VoteAggregate<T: Config> {
+    /// Default is empty vectors for all votes
+    pub struct VoteAggregate<AccountId, BlockNumber> {
         /// The current set of voters that approved it.
-        ayes: Vec<AccountIdFor<T>>,
+        ayes: Vec<AccountId>,
         /// The current set of voters that rejected it.
-        nays: Vec<AccountIdFor<T>>,
+        nays: Vec<AccountId>,
+        /// The current set of votes abstaining.
+        abstentions: Vec<AccountId>,
         /// The hard end time of this vote.
-        end: BlockNumberFor<T>,
+        end: BlockNumber,
+    }
+
+    impl<AccountId: Default + PartialEq, BlockNumber: Default> VoteAggregate<AccountId, BlockNumber> {
+        pub fn new(
+            ayes: Vec<AccountId>,
+            nays: Vec<AccountId>,
+            abstentions: Vec<AccountId>,
+            end: BlockNumber,
+        ) -> Self {
+            Self {
+                ayes,
+                nays,
+                abstentions,
+                end,
+            }
+        }
+
+        pub fn new_with_end(end: BlockNumber) -> Self {
+            Self {
+                end,
+                ..Default::default()
+            }
+        }
+
+        // This does not check if a vote is a duplicate, This must be done before calling this function
+        pub fn cast_vote(&mut self, voter: AccountId, vote: &Vote) {
+            match vote {
+                Vote::Aye => self.ayes.push(voter),
+                Vote::Nay => self.nays.push(voter),
+                Vote::Abstain => self.abstentions.push(voter),
+            }
+        }
+
+        pub fn remove_voters(&mut self, voters: &[AccountId]) {
+            self.ayes.retain(|x| !voters.contains(x));
+            self.nays.retain(|x| !voters.contains(x));
+            self.abstentions.retain(|x| !voters.contains(x));
+        }
+
+        pub fn has_voted(&self, voter: &AccountId) -> bool {
+            self.ayes.contains(voter) | self.nays.contains(voter) | self.abstentions.contains(voter)
+        }
     }
 
     #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
@@ -120,30 +169,72 @@ pub mod pallet {
     pub type Proposals<T: Config> =
         StorageMap<_, Blake2_128Concat, HashFor<T>, Proposal<T>, OptionQuery>;
 
+    #[pallet::storage]
+    /// Stores a vector of account IDs of current committee members
+    pub type Members<T: Config> = StorageValue<_, Vec<AccountIdFor<T>>, ValueQuery>;
+
+    #[pallet::storage]
+    /// Store a mapping (hash) -> VoteAggregate for all existing proposals.
+    pub type Votes<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        HashFor<T>,
+        VoteAggregate<AccountIdFor<T>, BlockNumberFor<T>>,
+        OptionQuery,
+    >;
+
     // end storage defs
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
+        /// A new proposal has been created
+        /// \[proposer_address, proposal_nonce, proposal_hash\]
         Proposed(AccountIdFor<T>, T::ProposalNonce, T::Hash),
+        /// A vote was cast
+        /// \[voter_address, proposal_hash, vote\]
+        VoteCast(AccountIdFor<T>, T::Hash, Vote),
     }
 
     #[pallet::error]
     pub enum Error<T> {
+        /// The origin making the call is not a member and it is a requirement that they are
+        NotMember,
+        /// Member has attempted to vote multiple times on a single proposal
+        DuplicateVote,
+        /// Attempted to cast a vote outside the accepted voting period for a proposal
+        NotInVotingPeriod,
+        /// The hash provided does not have an associated proposal
+        NoProposalWithHash,
         /// The data type for enumerating the proposals has reached its upper bound.
         /// No more proposals can be made
         ProposalNonceExhausted,
+        /// There was a numerical overflow or underflow in calculating when the voting period
+        /// should end
+        InvalidOperationInEndBlockComputation,
     }
 
     #[pallet::hooks]
-    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn on_initialize(n: BlockNumberFor<T>) -> Weight {
+            // perform upkeep only at the start of a new cycle
+            if Self::get_next_voting_period_end(&n)
+                == Ok(n + T::VotingPeriod::get() + T::ProposalSubmissionPeriod::get())
+            {
+                Self::upkeep(n);
+            }
+            0 // TODO: Calcualte the non-negotiable weight consumed by performing upkeep
+        }
+    }
 
     impl<T: Config> Pallet<T> {
+        /// Gets a new unused proposal nonce and increments the nonce in the store
+        /// Returns an error if the data type used for the nonce exceeds is maximum value
         fn take_and_increment_nonce() -> Result<T::ProposalNonce, Error<T>> {
-            let nonce = <ProposalCount<T>>::get();
+            let nonce = ProposalCount::<T>::get();
             match nonce.checked_add(&T::ProposalNonce::one()) {
                 Some(next) => {
-                    <ProposalCount<T>>::set(next);
+                    ProposalCount::<T>::set(next);
                     Ok(nonce)
                 }
                 None => Err(Error::ProposalNonceExhausted),
@@ -151,17 +242,76 @@ pub mod pallet {
         }
 
         pub fn active_proposals() -> Vec<HashFor<T>> {
-            <ActiveProposals<T>>::get()
+            ActiveProposals::<T>::get()
         }
 
         pub fn get_proposal(hash: &HashFor<T>) -> Option<Proposal<T>> {
-            <Proposals<T>>::get(hash)
+            Proposals::<T>::get(hash)
+        }
+
+        /// Get the votes for a proposal. Returns None if no proposal exists
+        pub fn get_votes_for(
+            hash: &HashFor<T>,
+        ) -> Option<VoteAggregate<AccountIdFor<T>, BlockNumberFor<T>>> {
+            Votes::<T>::get(hash)
+        }
+
+        pub fn members() -> Vec<AccountIdFor<T>> {
+            Members::<T>::get()
+        }
+
+        /// Used to check if an origin is signed and the signer is a member of
+        /// the committee
+        pub fn ensure_member(origin: OriginFor<T>) -> Result<AccountIdFor<T>, DispatchError> {
+            let who = ensure_signed(origin)?;
+            let members = Self::members();
+            ensure!(members.contains(&who), Error::<T>::NotMember);
+            Ok(who)
+        }
+
+        /// Returns the block at the end of the next voting period
+        pub fn get_next_voting_period_end(
+            block_number: &BlockNumberFor<T>,
+        ) -> Result<BlockNumberFor<T>, DispatchError> {
+            utils::get_vote_end(
+                block_number,
+                &T::VotingPeriod::get(),
+                &T::ProposalSubmissionPeriod::get(),
+            )
+            .ok_or_else(|| Error::<T>::InvalidOperationInEndBlockComputation.into())
+        }
+
+        /// Return true if the current block indicates it is the voting period
+        /// for the given VoteAggregate.
+        pub fn within_voting_period(
+            votes: &VoteAggregate<AccountIdFor<T>, BlockNumberFor<T>>,
+        ) -> bool {
+            let current_block = frame_system::Pallet::<T>::block_number();
+            current_block < votes.end && current_block >= votes.end - T::VotingPeriod::get()
+        }
+
+        /// Function executed at the initialization of the first block in
+        /// a new voting period cycle. Used to maintain the active proposals store.
+        fn upkeep(n: BlockNumberFor<T>) {
+            // clear out proposals that are no longer active
+            ActiveProposals::<T>::mutate(|proposals| {
+                proposals.retain(|hash| {
+                    if let Some(votes) = Self::get_votes_for(hash) {
+                        votes.end > n
+                    } else {
+                        false
+                    }
+                })
+            })
         }
     }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         #[pallet::weight(10_000)] // TODO: Set weights
+        /// Extrinsic to propose a new action to be voted upon in the next voting period.
+        /// The provided action will be turned into a proposal and added to the list of current active proposals
+        /// to be voted on in the next voting period.
         pub fn propose(origin: OriginFor<T>, action: Box<T::Action>) -> DispatchResultWithPostInfo {
             let proposer = ensure_signed(origin.clone())?;
             T::ProposalSubmissionOrigin::ensure_origin(origin)?;
@@ -173,14 +323,81 @@ pub mod pallet {
             let proposal_hash = proposal.hash();
 
             // Store the proposal by its hash.
-            <Proposals<T>>::insert(proposal_hash, proposal);
+            Proposals::<T>::insert(proposal_hash, proposal);
 
-            // Add the proposal to the active proposals
-            <ActiveProposals<T>>::append(&proposal_hash);
+            // Add the proposal to the active proposals and set the initial votes
+            // Set the end block number to the end of the next voting period
+            ActiveProposals::<T>::append(&proposal_hash);
+            let end = Self::get_next_voting_period_end(&frame_system::Pallet::<T>::block_number())?;
+
+            Votes::<T>::insert(proposal_hash, VoteAggregate::new_with_end(end));
 
             Self::deposit_event(Event::Proposed(proposer, nonce, proposal_hash));
 
             Ok(().into())
+        }
+
+        #[pallet::weight(10_000)] // TODO: Set weights
+        /// Extrinsic to vote on an existing proposal.
+        /// This can only be called by members of the committee.
+        /// Successfully cast votes will be recorded in the state and a proposal
+        /// meeting voting requirements can be executed.
+        pub fn vote(
+            origin: OriginFor<T>,
+            proposal_hash: HashFor<T>,
+            vote: Vote,
+        ) -> DispatchResultWithPostInfo {
+            // Only members can vote
+            let voter = Self::ensure_member(origin)?;
+
+            Votes::<T>::try_mutate(&proposal_hash, |votes| {
+                if let Some(votes) = votes {
+                    // Can only vote within the allowed range of blocks for this proposal
+                    ensure!(
+                        Self::within_voting_period(&votes),
+                        Error::<T>::NotInVotingPeriod
+                    );
+                    // members can vote only once
+                    ensure!(!votes.has_voted(&voter), Error::<T>::DuplicateVote);
+                    votes.cast_vote(voter.clone(), &vote); // mutates votes in place
+                    Self::deposit_event(Event::VoteCast(voter, proposal_hash, vote));
+                    Ok(())
+                } else {
+                    Err(Error::<T>::NoProposalWithHash)
+                }
+            })?;
+
+            Ok(().into())
+        }
+    }
+
+    impl<T: Config> InitializeMembers<AccountIdFor<T>> for Pallet<T> {
+        fn initialize_members(members: &[AccountIdFor<T>]) {
+            if !members.is_empty() {
+                assert!(
+                    Members::<T>::get().is_empty(),
+                    "Members are already initialized!"
+                );
+                Members::<T>::put(members);
+            }
+        }
+    }
+
+    impl<T: Config> ChangeMembers<AccountIdFor<T>> for Pallet<T> {
+        fn change_members_sorted(
+            _incoming: &[AccountIdFor<T>],
+            outgoing: &[AccountIdFor<T>],
+            new: &[AccountIdFor<T>],
+        ) {
+            // Remove outgoing members from any currently active votes
+            for proposal_hash in ActiveProposals::<T>::get() {
+                Votes::<T>::mutate(&proposal_hash, |votes| {
+                    if let Some(votes) = votes {
+                        votes.remove_voters(outgoing); // mutates votes in place
+                    }
+                });
+            }
+            Members::<T>::put(new);
         }
     }
 }
---
+++ pallets/committee/src/utils.rs
@@ -0,0 +1,88 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+use sp_runtime::traits::{CheckedAdd, CheckedDiv, CheckedMul, One};
+
+// Proposal submissions and voting follow set cycles e.g.
+//
+// ---v0--||-----s1----|--v1--||-----s2----|--v2--|...
+//
+// Proposals submitted in s1 are voted upon in v1 and after that
+// they may or may not be executed and then are dropped from the ActiveProposals set.
+//
+// Proposals submitted during v0 fall into the next submission period and
+// should be voted on in v1. To simplify implementation we assume the cycle begins with
+// an initial dummy voting period.
+//
+// Will return an None if any of the arithmetic operations fail due to overflow/underflow
+//
+pub fn get_vote_end<T: CheckedAdd + CheckedMul + CheckedDiv + One>(
+    current_block: &T,
+    voting_period: &T,
+    proposal_period: &T,
+) -> Option<T> {
+    let epoch_period = voting_period.checked_add(proposal_period)?;
+
+    // [(current_block // period) + 1] * period + voting_period
+    // return the block at the end of the next voting period after the current one
+    current_block
+        .checked_div(&epoch_period)?
+        .checked_add(&T::one())?
+        .checked_mul(&epoch_period)?
+        .checked_add(&voting_period)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter;
+
+    const VOTE_P: i32 = 2;
+    const PROPOSAL_P: i32 = 3;
+
+    #[test]
+    // A proposal made during the start dummpy period must have votes submitted before
+    // the end of v1
+    fn test_proposal_in_v0() {
+        assert_eq!(
+            get_vote_end(&0, &VOTE_P, &PROPOSAL_P),
+            Some(VOTE_P + PROPOSAL_P + VOTE_P)
+        )
+    }
+
+    #[test]
+    // A proposal made during s1 must have votes submitted before
+    // the end of v1
+    fn test_proposal_in_s1() {
+        assert_eq!(
+            get_vote_end(&4, &VOTE_P, &PROPOSAL_P),
+            Some(VOTE_P + PROPOSAL_P + VOTE_P)
+        )
+    }
+
+    #[test]
+    // A proposal made during v1 must have votes submitted before
+    // the end of v2
+    fn test_proposal_in_v1() {
+        assert_eq!(
+            get_vote_end(&9, &VOTE_P, &PROPOSAL_P),
+            Some(VOTE_P + 2 * (PROPOSAL_P + VOTE_P))
+        )
+    }
+
+    #[test]
+    // Check a range of blocks are as expected
+    fn test_proposal_range() {
+        let result: Vec<i32> = (0..15)
+            .map(|i| get_vote_end(&i, &VOTE_P, &PROPOSAL_P).unwrap())
+            .collect();
+
+        let expected: Vec<i32> = iter::empty()
+            .chain(iter::repeat(VOTE_P + PROPOSAL_P + VOTE_P).take(5))
+            .chain(iter::repeat(VOTE_P + 2 * (PROPOSAL_P + VOTE_P)).take(5))
+            .chain(iter::repeat(VOTE_P + 3 * (PROPOSAL_P + VOTE_P)).take(5))
+            .collect();
+
+        assert_eq!(result, expected)
+    }
+}
