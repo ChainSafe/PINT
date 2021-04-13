3a360338280d65010882234cb5b87d29b60a4e97
Willem Olding <willemolding@gmail.com>
2021-04-14 00:15:30 +1000

    feat(saftregistry): add SaftRegistry pallet (#22)
    * feat:pallets/saft-registry: Adds SAFT registry pallet with extrinsics
    
    * feat:pallets/saft-registry: Adds saft registry and tests
    
    * feat:pallets/saft-registry: Adds call to recorder on nav update
    
    * chore:pallets/saft-registry: cargo fmt
    
    * chore:pallets/saft-registry: Tidy event docstrings. Include NAV in update event
    
    * chore:pallets/saft-registry: Fixes clippy warning. Makes clippy error on warnings
    
    * chore:pallets/saft-registry: fmt
    
    * Update .github/workflows/test-code.yml

---
+++ pallets/saft-registry/src/lib.rs
@@ -0,0 +1,160 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+mod traits;
+
+#[frame_support::pallet]
+// this is requires as the #[pallet::event] proc macro generates code that violates this lint
+#[allow(clippy::unused_unit)]
+pub mod pallet {
+    use crate::traits::{AssetAvailability, AssetRecorder};
+    use frame_support::{
+        dispatch::DispatchResultWithPostInfo, pallet_prelude::*,
+        sp_runtime::traits::AtLeast32BitUnsigned,
+    };
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        // Origin that is allowed to manage the SAFTs
+        type AdminOrigin: EnsureOrigin<Self::Origin>;
+        type AssetRecorder: AssetRecorder<Self::AssetId, Self::Balance>;
+        type Balance: Parameter + AtLeast32BitUnsigned;
+        type AssetId: Parameter;
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
+
+    #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
+    pub struct SAFTRecord<Balance, NAV> {
+        nav: NAV,
+        units: Balance,
+    }
+
+    impl<Balance, NAV> SAFTRecord<Balance, NAV> {
+        pub fn new(nav: NAV, units: Balance) -> Self {
+            Self { nav, units }
+        }
+    }
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    #[pallet::storage]
+    /// Store a mapping (AssetId) -> Vec<SaftRecord> for all active SAFTs
+    pub type ActiveSAFTs<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        T::AssetId,
+        Vec<SAFTRecord<T::Balance, T::Balance>>,
+        ValueQuery,
+    >;
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// A new SAFT was added
+        /// \[AssetId, AssetIndex\]
+        SAFTAdded(T::AssetId, u32),
+        /// A SAFT was removed
+        /// \[AssetId, AssetIndex\]
+        SAFTRemoved(T::AssetId, u32),
+        /// The NAV for a SAFT was updated
+        /// \[AssetId, AssetIndex, OldNav, NewNav\]
+        NavUpdated(T::AssetId, u32, T::Balance, T::Balance),
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        // No SAFT with the given index exists for the given AssetId
+        AssetIndexOutOfBounds,
+    }
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(10_000)] // TODO: Set weights
+        pub fn add_saft(
+            origin: OriginFor<T>,
+            asset_id: T::AssetId,
+            nav: T::Balance,
+            units: T::Balance,
+        ) -> DispatchResultWithPostInfo {
+            T::AdminOrigin::ensure_origin(origin)?;
+
+            <T as Config>::AssetRecorder::add_asset(
+                &asset_id,
+                &units,
+                &AssetAvailability::SAFT,
+                &nav,
+            )?;
+            ActiveSAFTs::<T>::append(asset_id.clone(), SAFTRecord::new(nav, units));
+            Self::deposit_event(Event::<T>::SAFTAdded(asset_id, 0));
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)] // TODO: Set weights
+        pub fn remove_saft(
+            origin: OriginFor<T>,
+            asset_id: T::AssetId,
+            index: u32,
+        ) -> DispatchResultWithPostInfo {
+            T::AdminOrigin::ensure_origin(origin)?;
+            let index_usize: usize = index as usize;
+            ActiveSAFTs::<T>::try_mutate(asset_id.clone(), |safts| -> Result<(), DispatchError> {
+                if index_usize >= safts.len() {
+                    Err(Error::<T>::AssetIndexOutOfBounds.into())
+                } else {
+                    <T as Config>::AssetRecorder::remove_asset(&asset_id)?;
+                    safts.remove(index_usize);
+                    Self::deposit_event(Event::<T>::SAFTRemoved(asset_id, index));
+
+                    Ok(())
+                }
+            })?;
+
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000)] // TODO: Set weights
+        /// Called to update the Net Asset Value (NAV) associated with
+        /// a SAFT record in the registry
+        pub fn report_nav(
+            origin: OriginFor<T>,
+            asset_id: T::AssetId,
+            index: u32,
+            latest_nav: T::Balance,
+        ) -> DispatchResultWithPostInfo {
+            T::AdminOrigin::ensure_origin(origin)?;
+            let index_usize: usize = index as usize;
+            ActiveSAFTs::<T>::try_mutate(asset_id.clone(), |safts| -> Result<(), DispatchError> {
+                if let Some(mut nav_record) = safts.get_mut(index_usize) {
+                    let prev_nav = nav_record.nav.clone();
+                    nav_record.nav = latest_nav.clone();
+                    <T as Config>::AssetRecorder::update_nav(&asset_id, &latest_nav)?;
+                    Self::deposit_event(Event::<T>::NavUpdated(
+                        asset_id, index, prev_nav, latest_nav,
+                    ));
+                    Ok(())
+                } else {
+                    // get_mut will return None if index out of bounds
+                    Err(Error::<T>::AssetIndexOutOfBounds.into())
+                }
+            })?;
+            Ok(().into())
+        }
+    }
+}
---
+++ pallets/saft-registry/src/mock.rs
@@ -0,0 +1,106 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+// Required as construct_runtime! produces code that violates this lint
+#![allow(clippy::from_over_into)]
+
+use crate as pallet_saft_registry;
+use frame_support::{ord_parameter_types, parameter_types};
+use frame_system as system;
+use pallet_saft_registry::traits::{AssetAvailability, AssetRecorder};
+
+use sp_core::H256;
+use sp_runtime::{
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
+    DispatchError,
+};
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Module, Call, Config, Storage, Event<T>},
+        SaftRegistry: pallet_saft_registry::{Module, Call, Storage, Event<T>},
+    }
+);
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub const SS58Prefix: u8 = 42;
+}
+
+pub(crate) type Balance = u64;
+pub(crate) type AccountId = u64;
+
+impl system::Config for Test {
+    type BaseCallFilter = ();
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type Origin = Origin;
+    type Call = Call;
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = Event;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = ();
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = SS58Prefix;
+}
+
+pub(crate) const ADMIN_ACCOUNT_ID: AccountId = 88;
+
+ord_parameter_types! {
+    pub const AdminAccountId: AccountId = ADMIN_ACCOUNT_ID;
+}
+
+pub struct MockAssetRecorder();
+
+impl<AssetId, Balance> AssetRecorder<AssetId, Balance> for MockAssetRecorder {
+    fn add_asset(
+        _: &AssetId,
+        _: &Balance,
+        _: &AssetAvailability,
+        _: &Balance,
+    ) -> Result<(), DispatchError> {
+        Ok(())
+    }
+    fn remove_asset(_: &AssetId) -> Result<(), DispatchError> {
+        Ok(())
+    }
+    fn update_nav(_: &AssetId, _: &Balance) -> Result<(), DispatchError> {
+        Ok(())
+    }
+}
+
+impl pallet_saft_registry::Config for Test {
+    type AdminOrigin = frame_system::EnsureSignedBy<AdminAccountId, AccountId>;
+    type Event = Event;
+    type Balance = Balance;
+    type AssetRecorder = MockAssetRecorder;
+    type AssetId = u32;
+}
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    let t = frame_system::GenesisConfig::default()
+        .build_storage::<Test>()
+        .unwrap();
+    t.into()
+}
