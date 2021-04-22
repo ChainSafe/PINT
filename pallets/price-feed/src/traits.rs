ea98b51e0bbb20b5a612b03295d526a598b1f5f5
Matthias Seitz <matthias.seitz@outlook.de>
2021-04-22 15:55:43 +0200

    Add price feed pallet (#40)
    * chore(deps): cargo update --workspace
    
    * fix(deps): update renamed xcm crate names
    
    * fix: rename ModuleId to PalletId
    
    * feat:pallets/orcale: add oracle pallet template
    
    * feat:pallets/orcale: rename to price feed and include chainlink feed
    
    * feat:pallets/price-feed: integrate latest chainlink price feed pallet
    
    * rustfmt
    
    * chore(deps): sync with chainlink deps
    
    * chore(deps): sort members
    
    * feat:pallets/price-feed: add types
    
    * feat:pallets/price-feed: introduce price feed types and price feed trait
    
    * feat:pallets/price-feed: empty price feed trait implementation
    
    * rustfmt
    
    * chore(deps): enable std feature for balances
    
    * feat:pallets/price-feed: add mock config
    
    * docs:pallets/price-feed: clarify notes on precision
    
    * test:pallets/price-feed: add basic feed creation test
    
    * rustfmt
    
    * Update pallets/price-feed/src/lib.rs
    
    Co-authored-by: Willem Olding <willemolding@gmail.com>
    
    * feat:pallets/price-feed: add mapping from assetId to feedId
    
    * feat:pallets/price-feed: add genesis build support
    
    * refactor:pallets/price-feed: replace generic price type with FixedU128
    
    * feat:pallets/price-feed: implement get_price_pair
    
    * rustfmt
    
    * chore(clippy): make clippy happy
    
    * chore(deps): update chainlink feed pallet
    
    * feat:pallets/price-feed: drop unused type
    
    * test:pallets/price-feed: update mock template
    
    * test:pallets/price-feed: add price pair tests
    
    * test:pallets/price-feed: reexport feedbuilder for tests
    
    * test:pallets/price-feed: add price feed tests
    
    * chore:pallets/price-feed: remove commented out config typ eartefacts
    
    Co-authored-by: Willem Olding <willemolding@gmail.com>

---
+++ pallets/price-feed/src/traits.rs
@@ -0,0 +1,17 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+use crate::types::AssetPricePair;
+use frame_support::dispatch::DispatchError;
+
+/// An interface to access price data
+pub trait PriceFeed<AssetId> {
+    /// Returns the current price pair for `base/quote` where `base` is the native token
+    fn get_price(quote: AssetId) -> Result<AssetPricePair<AssetId>, DispatchError>;
+
+    /// Returns the current price pair for `base/quote`
+    fn get_price_pair(
+        base: AssetId,
+        quote: AssetId,
+    ) -> Result<AssetPricePair<AssetId>, DispatchError>;
+}
