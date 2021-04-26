bfbadf009139654d9296879834ac01484a7f2ac6
Matthias Seitz <matthias.seitz@outlook.de>
2021-04-26 17:34:53 +0200

    feat:pallets/asset-depository: add asset depository pallet

---
+++ pallets/asset-depository/src/traits.rs
@@ -0,0 +1,14 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+use frame_support::sp_runtime::DispatchResult;
+
+/// An abstraction over the multiple balances for different assets
+pub trait MultiAssetDepository<AssetId, AccountId, Balance> {
+
+    /// Add `amount` to the balance of `who` under `asset_id`.
+    fn deposit(asset_id: &AssetId, who: &AccountId, amount: Balance) -> DispatchResult;
+
+    /// Remove `amount` from the balance of `who` under `asset_id`.
+    fn withdraw(asset_id: &AssetId, who: &AccountId, amount: Balance) -> DispatchResult;
+}
---
+++ pallets/asset-depository/src/types.rs
@@ -0,0 +1,29 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+use frame_support::sp_runtime::{traits::Saturating, RuntimeDebug};
+use codec::{Encode, Decode};
+
+/// Represents the balance of a single asset.
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Default, RuntimeDebug)]
+pub struct AccountBalance<Balance> {
+    /// Non-reserved part of the balance. There may still be restrictions on
+    /// this, but it is the total pool what may in principle be transferred,
+    /// reserved.
+    ///
+    /// This is the only balance that matters in terms of most operations on
+    /// tokens.
+    pub available: Balance,
+    /// Balance which is currently locked and can't be accessed by the user.
+    ///
+    /// This is intended to reserve an amount of this asset for PINT related operations, so that it can be spend.
+    pub reserved: Balance,
+}
+
+
+impl<Balance: Saturating + Copy + Ord> AccountBalance<Balance> {
+    /// The total balance that is currently reserved and available
+    pub fn total_balance(&self) -> Balance {
+        self.available.saturating_add(self.reserved)
+    }
+}
