7e50922c1d96dd8e69a68d020f0a7dc58d221d89
Matthias Seitz <matthias.seitz@outlook.de>
2021-05-13 12:10:55 +0200

    feat(index): add fee trait

--- pallets/asset-index/src/traits.rs
+++ pallets/asset-index/src/traits.rs
@@ -3,6 +3,7 @@
 
 pub use crate::types::AssetAvailability;
 use frame_support::dispatch::DispatchResult;
+use frame_support::sp_runtime::traits::AtLeast32BitUnsigned;
 use xcm::v0::MultiLocation;
 
 pub trait AssetRecorder<AssetId, Balance> {
@@ -17,10 +18,24 @@ pub trait AssetRecorder<AssetId, Balance> {
     fn remove_asset(id: &AssetId) -> DispatchResult;
 }
 
+/// Type that provides the mapping between `AssetId` and `MultiLocation`.
 pub trait MultiAssetRegistry<AssetId> {
     /// Determines the relative location of the consensus system where the given asset is native from the point of view of the current system
     fn native_asset_location(asset: &AssetId) -> Option<MultiLocation>;
 
     /// Whether the given identifier is currently supported as a liquid asset
     fn is_liquid_asset(asset: &AssetId) -> bool;
+}
+
+/// Type that calculations any fees to be deducted for every withdrawal.
+pub trait WithdrawalFee<Balance> {
+    /// Calculates the fee to be deducted from the PINT being redeemed
+    // TODO specify parameters
+    fn withdrawal_fee(amount: Balance) -> Balance;
+}
+
+impl<Balance: AtLeast32BitUnsigned> WithdrawalFee<Balance> for () {
+    fn withdrawal_fee(_: Balance) -> Balance {
+        Balance::zero()
+    }
 }
