78e1fc7013aeb291d6a5ffea136119489abf83ac
clearloop <udtrokia@gmail.com>
2021-05-27 22:41:13 +0800

    feat(js): package launch

--- js/launch/README.md
+++ js/launch/README.md
@@ -1,7 +1,18 @@
-# E2E
+# PINT launch
 
-E2E tests for PINT
+Launch PINT with polkadot and statemint
 
+```typescript
+import { local, docker } from "@pint/launch";
+import { StdioOptions } from "child_process";
+
+async main() {
+    const stdio = "inherit";
+    const localPs: ChildProcess = await local(stdio);
+    const dockerPs: ChildProcess = await local(stdio);
+}
+
+```
 
 ## LICENSE
 
--- js/launch/index.ts
+++ js/launch/index.ts
@@ -1,42 +1,25 @@
 /**
  * pint-launch
  */
-import { spawn, spawnSync, execSync } from "child_process";
+import findUp from "find-up";
 import path from "path";
-
-/**
- * Launch PINT locally or using docker in CI
- */
-async function launch(docker: boolean = false) {
-    if (!docker) {
-        await local();
-    }
-}
+import { fork, ChildProcess, StdioOptions, spawn } from "child_process";
 
 /**
  * Launch PINT locally
- *
- * required bins:
- *  - npm
- *  - node
  */
-async function local() {
-    // build polkadot-launch
-    execSync("npm run build", {
-        cwd: path.resolve(__dirname, "../polkadot-launch"),
-        stdio: "inherit",
+export async function local(stdio?: StdioOptions): Promise<ChildProcess> {
+    return fork("js/polkadot-launch", ["config.json"], {
+        cwd: path.resolve(await findUp("Cargo.toml"), ".."),
+        stdio,
     });
-    spawnSync(
-        "node",
-        [
-            path.resolve(__dirname, "../polkadot-launch/dist/index.js"),
-            path.resolve(__dirname, "../../config.json"),
-        ],
-        {
-            stdio: "inherit",
-        }
-    );
 }
-// async function docker() {}
 
-launch();
+/**
+ * Launch PINT via docker (CI)
+ */
+export async function docker(stdio?: StdioOptions): Promise<ChildProcess> {
+    return spawn("docker", ["run", "-it", "launch"], {
+        stdio,
+    });
+}
