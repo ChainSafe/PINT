9197ec915cbd49618e896d1f70b38e03d400eddd
clearloop <udtrokia@gmail.com>
2021-06-17 15:08:46 +0800

    feat(js): add chainlink types

--- js/pint-types-bundle/index.ts
+++ js/pint-types-bundle/index.ts
@@ -13,14 +13,32 @@ export const definitions = {
                 LookupSource: "MultiAddress",
                 AssetId: "u32",
                 AccountIdFor: "AccountId",
-                Balance: "u128",
-                BalanceFor: "Balance",
-                FeedId: "u64",
-                HashFor: "Hash",
+                AccountBalance: {
+                    available: "Balance",
+                    reserved: "Balance",
+                },
                 AssetAvailability: {
                     _enum: {
                         Liquid: "MultiLocation",
                         Saft: null,
+                    },
+                },
+                Balance: "u128",
+                BalanceFor: "Balance",
+                CommitteeMember: {
+                    account_id: "AccountId",
+                    member_type: "MemberType",
+                },
+                FeedId: "u64",
+                HashFor: "Hash",
+                IndexAssetData: {
+                    units: "Balance",
+                    availability: "AssetAvailability",
+                },
+                MemberType: {
+                    _enum: {
+                        Council: null,
+                        Constituent: null,
                     },
                 },
             },
