4e63e7feb38f40ca95c73e2b0f2ba47e5074f86d
Matthias Seitz <matthias.seitz@outlook.de>
2021-07-07 11:03:17 +0200

    implement staking

--- test-utils/xcm-test-support/src/relay.rs
+++ test-utils/xcm-test-support/src/relay.rs
@@ -1,9 +1,12 @@
 // Copyright 2021 ChainSafe Systems
 // SPDX-License-Identifier: LGPL-3.0-only
 
+use std::{cell::RefCell, collections::HashSet};
+
 use codec::{Decode, Encode};
 use frame_support::{
     construct_runtime, parameter_types,
+    sp_runtime::traits::BlakeTwo256,
     traits::{
         All, Currency, FindAuthor, Imbalance, InstanceFilter, MaxEncodedLen, OnUnbalanced,
         OneSessionHandler,
@@ -11,7 +14,11 @@ use frame_support::{
     weights::Weight,
 };
 use sp_core::H256;
-use sp_runtime::testing::Header;
+use sp_runtime::{
+    curve::PiecewiseLinear,
+    testing::{Header, TestXt, UintAuthorityId},
+    Perbill,
+};
 
 use cumulus_primitives_core::ParaId;
 use frame_election_provider_support::onchain;
@@ -29,10 +36,7 @@ use xcm_builder::{
 use xcm_executor::{Config, XcmExecutor};
 
 use super::types::*;
-
-parameter_types! {
-    pub const BlockHashCount: u64 = 250;
-}
+use frame_support::sp_runtime::AccountId32;
 
 impl frame_system::Config for Runtime {
     type Origin = Origin;
@@ -58,24 +62,6 @@ impl frame_system::Config for Runtime {
     type SystemWeightInfo = ();
     type SS58Prefix = ();
     type OnSetCode = ();
-}
-
-parameter_types! {
-    pub ExistentialDeposit: Balance = 420;
-    pub const MaxLocks: u32 = 50;
-    pub const MaxReserves: u32 = 50;
-}
-
-impl pallet_balances::Config for Runtime {
-    type MaxLocks = MaxLocks;
-    type Balance = Balance;
-    type Event = Event;
-    type DustRemoval = ();
-    type ExistentialDeposit = ExistentialDeposit;
-    type AccountStore = System;
-    type WeightInfo = ();
-    type MaxReserves = MaxReserves;
-    type ReserveIdentifier = [u8; 8];
 }
 
 impl shared::Config for Runtime {}
@@ -155,6 +141,266 @@ impl ump::Config for Runtime {
 
 impl origin::Config for Runtime {}
 
+// -- staking support
+
+thread_local! {
+    static SESSION: RefCell<(Vec<AccountId>, HashSet<AccountId>)> = RefCell::new(Default::default());
+}
+
+type NegativeImbalanceOf<T> = <<T as pallet_staking::Config>::Currency as Currency<
+    <T as frame_system::Config>::AccountId,
+>>::NegativeImbalance;
+
+/// Another session handler struct to test on_disabled.
+pub struct OtherSessionHandler;
+impl OneSessionHandler<AccountId> for OtherSessionHandler {
+    type Key = UintAuthorityId;
+
+    fn on_genesis_session<'a, I: 'a>(_: I)
+    where
+        I: Iterator<Item = (&'a AccountId, Self::Key)>,
+        AccountId: 'a,
+    {
+    }
+
+    fn on_new_session<'a, I: 'a>(_: bool, validators: I, _: I)
+    where
+        I: Iterator<Item = (&'a AccountId, Self::Key)>,
+        AccountId: 'a,
+    {
+        SESSION.with(|x| {
+            *x.borrow_mut() = (validators.map(|x| x.0.clone()).collect(), HashSet::new())
+        });
+    }
+
+    fn on_disabled(validator_index: usize) {
+        SESSION.with(|d| {
+            let mut d = d.borrow_mut();
+            let value = d.0[validator_index].clone();
+            d.1.insert(value);
+        })
+    }
+}
+
+impl sp_runtime::BoundToRuntimeAppPublic for OtherSessionHandler {
+    type Public = UintAuthorityId;
+}
+
+/// Author of block is always 11
+pub struct Author11;
+impl FindAuthor<AccountId> for Author11 {
+    fn find_author<'a, I>(_digests: I) -> Option<AccountId>
+    where
+        I: 'a + IntoIterator<Item = (frame_support::ConsensusEngineId, &'a [u8])>,
+    {
+        Some(AccountId32::new([11u8; 32]))
+    }
+}
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub BlockWeights: frame_system::limits::BlockWeights =
+        frame_system::limits::BlockWeights::simple_max(
+            frame_support::weights::constants::WEIGHT_PER_SECOND * 2
+        );
+    pub static SessionsPerEra: sp_staking::SessionIndex = 3;
+    pub static ExistentialDeposit: Balance = 1;
+    pub static SlashDeferDuration: EraIndex = 0;
+    pub static Period: BlockNumber = 5;
+    pub static Offset: BlockNumber = 0;
+
+    pub const MaxLocks: u32 = 50;
+    pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Runtime {
+    type MaxLocks = MaxLocks;
+    type Balance = Balance;
+    type Event = Event;
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type MaxReserves = MaxReserves;
+    type ReserveIdentifier = [u8; 8];
+    type WeightInfo = ();
+}
+parameter_types! {
+    pub const UncleGenerations: u64 = 0;
+    pub const DisabledValidatorsThreshold: Perbill = Perbill::from_percent(25);
+}
+
+sp_runtime::impl_opaque_keys! {
+    pub struct SessionKeys {
+        pub other: OtherSessionHandler,
+    }
+}
+
+impl pallet_session::Config for Runtime {
+    type SessionManager = pallet_session::historical::NoteHistoricalRoot<Runtime, Staking>;
+    type Keys = SessionKeys;
+    type ShouldEndSession = pallet_session::PeriodicSessions<Period, Offset>;
+    type SessionHandler = (OtherSessionHandler,);
+    type Event = Event;
+    type ValidatorId = AccountId;
+    type ValidatorIdOf = pallet_staking::StashOf<Runtime>;
+    type DisabledValidatorsThreshold = DisabledValidatorsThreshold;
+    type NextSessionRotation = pallet_session::PeriodicSessions<Period, Offset>;
+    type WeightInfo = ();
+}
+
+impl pallet_session::historical::Config for Runtime {
+    type FullIdentification = pallet_staking::Exposure<AccountId, Balance>;
+    type FullIdentificationOf = pallet_staking::ExposureOf<Runtime>;
+}
+parameter_types! {
+    pub const MinimumPeriod: u64 = 5;
+}
+impl pallet_timestamp::Config for Runtime {
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type MinimumPeriod = MinimumPeriod;
+    type WeightInfo = ();
+}
+pallet_staking_reward_curve::build! {
+    const I_NPOS: PiecewiseLinear<'static> = curve!(
+        min_inflation: 0_025_000,
+        max_inflation: 0_100_000,
+        ideal_stake: 0_500_000,
+        falloff: 0_050_000,
+        max_piece_count: 40,
+        test_precision: 0_005_000,
+    );
+}
+
+parameter_types! {
+    pub const BondingDuration: EraIndex = 3;
+    pub const RewardCurve: &'static PiecewiseLinear<'static> = &I_NPOS;
+    pub const MaxNominatorRewardedPerValidator: u32 = 64;
+}
+
+thread_local! {
+    pub static REWARD_REMAINDER_UNBALANCED: RefCell<u128> = RefCell::new(0);
+}
+
+pub struct RewardRemainderMock;
+
+impl OnUnbalanced<NegativeImbalanceOf<Runtime>> for RewardRemainderMock {
+    fn on_nonzero_unbalanced(amount: NegativeImbalanceOf<Runtime>) {
+        REWARD_REMAINDER_UNBALANCED.with(|v| {
+            *v.borrow_mut() += amount.peek();
+        });
+        drop(amount);
+    }
+}
+
+impl onchain::Config for Runtime {
+    type AccountId = AccountId;
+    type BlockNumber = BlockNumber;
+    type BlockWeights = BlockWeights;
+    type Accuracy = Perbill;
+    type DataProvider = Staking;
+}
+impl staking::Config for Runtime {
+    const MAX_NOMINATIONS: u32 = 16;
+    type Currency = Balances;
+    type UnixTime = Timestamp;
+    type CurrencyToVote = frame_support::traits::SaturatingCurrencyToVote;
+    type RewardRemainder = RewardRemainderMock;
+    type Event = Event;
+    type Slash = ();
+    type Reward = ();
+    type SessionsPerEra = SessionsPerEra;
+    type SlashDeferDuration = SlashDeferDuration;
+    type SlashCancelOrigin = frame_system::EnsureRoot<Self::AccountId>;
+    type BondingDuration = BondingDuration;
+    type SessionInterface = Self;
+    type EraPayout = ConvertCurve<RewardCurve>;
+    type NextNewSession = Session;
+    type MaxNominatorRewardedPerValidator = MaxNominatorRewardedPerValidator;
+    type ElectionProvider = onchain::OnChainSequentialPhragmen<Self>;
+    type GenesisElectionProvider = Self::ElectionProvider;
+    type WeightInfo = ();
+}
+
+parameter_types! {
+    pub const ProxyDepositBase: Balance = 100;
+    pub const ProxyDepositFactor: Balance = 100;
+    pub const MaxProxies: u16 = 32;
+    pub const AnnouncementDepositBase: Balance = 100;
+    pub const AnnouncementDepositFactor: Balance = 100;
+    pub const MaxPending: u16 = 32;
+}
+
+/// The type used to represent the kinds of proxying allowed.
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Encode, Decode, Debug, MaxEncodedLen)]
+pub enum ProxyType {
+    Any = 0,
+    NonTransfer = 1,
+    Governance = 2,
+    Staking = 3,
+    // Skip 4 as it is now removed (was SudoBalances)
+    IdentityJudgement = 5,
+    CancelProxy = 6,
+}
+
+impl Default for ProxyType {
+    fn default() -> Self {
+        Self::Any
+    }
+}
+impl InstanceFilter<Call> for ProxyType {
+    fn filter(&self, _: &Call) -> bool {
+        true
+    }
+}
+
+impl pallet_proxy::Config for Runtime {
+    type Event = Event;
+    type Call = Call;
+    type Currency = Balances;
+    type ProxyType = ProxyType;
+    type ProxyDepositBase = ProxyDepositBase;
+    type ProxyDepositFactor = ProxyDepositFactor;
+    type MaxProxies = MaxProxies;
+    type WeightInfo = ();
+    type MaxPending = MaxPending;
+    type CallHasher = BlakeTwo256;
+    type AnnouncementDepositBase = AnnouncementDepositBase;
+    type AnnouncementDepositFactor = AnnouncementDepositFactor;
+}
+
+impl<LocalCall> frame_system::offchain::SendTransactionTypes<LocalCall> for Runtime
+where
+    Call: From<LocalCall>,
+{
+    type OverarchingCall = Call;
+    type Extrinsic = TestXt<Call, ()>;
+}
+
+parameter_types! {
+    pub const AssetDeposit: u64 = 1;
+    pub const ApprovalDeposit: u64 = 1;
+    pub const StringLimit: u32 = 50;
+    pub const MetadataDepositBase: u64 = 1;
+    pub const MetadataDepositPerByte: u64 = 1;
+}
+
+impl pallet_assets::Config for Runtime {
+    type Event = Event;
+    type Balance = Balance;
+    type AssetId = AssetId;
+    type Currency = Balances;
+    type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+    type AssetDeposit = AssetDeposit;
+    type MetadataDepositBase = MetadataDepositBase;
+    type MetadataDepositPerByte = MetadataDepositPerByte;
+    type ApprovalDeposit = ApprovalDeposit;
+    type StringLimit = StringLimit;
+    type Freezer = ();
+    type WeightInfo = ();
+    type Extra = ();
+}
+
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
 type Block = frame_system::mocking::MockBlock<Runtime>;
 
@@ -165,9 +411,21 @@ construct_runtime!(
         UncheckedExtrinsic = UncheckedExtrinsic,
     {
         System: frame_system::{Pallet, Call, Storage, Config, Event<T>},
+        Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
         Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
         ParasOrigin: origin::{Pallet, Origin},
         ParasUmp: ump::{Pallet, Call, Storage, Event},
         XcmPallet: pallet_xcm::{Pallet, Call, Storage, Event<T>},
+
+         // use polkadot index 7
+        Staking: staking::{Pallet, Call, Storage, Event<T>} = 7,
+
+        Session: pallet_session::{Pallet, Call, Storage, Event},
+
+        // use polkadot index 29
+        Proxy: pallet_proxy::{Pallet, Call, Storage, Event<T>} = 29,
+
+        // use statemint index 50
+        Assets: pallet_assets::{Pallet, Call, Storage, Event<T>} = 50,
     }
 );
