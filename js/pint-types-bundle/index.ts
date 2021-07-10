00945df5fa119dfb01690419ef61f64a4cfebd60
clearloop <udtrokia@gmail.com>
2021-07-11 00:28:38 +0800

    feat(types): migrate types.json in /node/build.rs

--- js/pint-types-bundle/index.ts
+++ js/pint-types-bundle/index.ts
@@ -2,99 +2,14 @@ import {
     OverrideBundleDefinition,
     OverrideBundleType,
 } from "@polkadot/types/types";
+import PINTTypes from "./pint.json";
 
 export const definitions = {
     types: [
         {
             // on all versions
             minmax: [0, undefined],
-            types: {
-                Address: "MultiAddress",
-                LookupSource: "MultiAddress",
-                LookupSourceFor: "LookupSource",
-                Action: "Call",
-                AssetId: "u32",
-                AccountIdFor: "AccountId",
-                AccountBalance: {
-                    available: "Balance",
-                    reserved: "Balance",
-                },
-                AssetAvailability: {
-                    _enum: {
-                        Liquid: "MultiLocation",
-                        Saft: null,
-                    },
-                },
-                AssetWithdrawal: {
-                    asset: "AssetId",
-                    state: "RedemptionState",
-                    units: "Balance",
-                },
-                Balance: "u128",
-                BalanceFor: "Balance",
-                CommitteeMember: {
-                    account_id: "AccountId",
-                    member_type: "MemberType",
-                },
-                CurrencyId: "AssetId",
-                CurrencyIdOf: "CurrencyId",
-                FeedId: "u64",
-                FeedIdFor: "FeedId",
-                HashFor: "Hash",
-                IndexAssetData: {
-                    units: "Balance",
-                    availability: "AssetAvailability",
-                },
-                MemberType: {
-                    _enum: {
-                        Council: null,
-                        Constituent: null,
-                    },
-                },
-                MemberVote: {
-                    member: "CommitteeMember",
-                    vote: "Vote",
-                },
-                PendingRedemption: {
-                    initiated: "BlockNumber",
-                    assets: "Vec<AssetWithdrawal>",
-                },
-                ProposalNonce: "u32",
-                ProxyType: {
-                    _enum: ["Any", "NonTransfer", "Governance", "Staking"],
-                },
-                ProxyState: {
-                    added: "Vec<ProxyType>",
-                },
-                RedemptionState: {
-                    _enum: {
-                        Initiated: null,
-                        Unbonding: null,
-                        Transferred: null,
-                    },
-                },
-                SAFTRecord: {
-                    nav: "Balance",
-                    units: "Balance",
-                },
-                StakingBondState: {
-                    controller: "LookupSourceFor",
-                    bonded: "Balance",
-                    unbonded: "Balance",
-                    unlocked_chunks: "u32",
-                },
-                Vote: {
-                    _enum: {
-                        Aye: null,
-                        Nay: null,
-                        Abstain: null,
-                    },
-                },
-                VoteAggregate: {
-                    votes: "Vec<MemberVote>",
-                    end: "BlockNumber",
-                },
-            },
+            types: PINTTypes,
         },
     ],
 } as OverrideBundleDefinition;
--- node/build.rs
+++ node/build.rs
@@ -1,9 +1,16 @@
 // Copyright 2021 ChainSafe Systems
 // SPDX-License-Identifier: LGPL-3.0-only
+use std::fs;
+
+const PINT_TYPES_SOURCE: &'static str = "../js/pint-types-bundle/pint.json";
+const PINT_TYPES_TARGET: &'static str = "../resources/types.json";
 
 use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
     generate_cargo_keys();
     rerun_if_git_head_changed();
+
+    // Generate PINT types
+    fs::copy(PINT_TYPES_SOURCE, PINT_TYPES_TARGET).expect("Generate PINT types failed");
 }
