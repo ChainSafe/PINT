c2d319686e85c5dabdd06a131dee22076975abf4
clearloop <udtrokia@gmail.com>
2021-08-20 08:55:32 +0800

    feat(e2e): get back annotated tests

--- js/e2e/index.ts
+++ js/e2e/index.ts
@@ -93,88 +93,88 @@ const TESTS = (api: ApiPromise, config: ExtrinsicConfig): Extrinsic[] => {
                 );
             },
         },
-        // {
-        //     required: ["priceFeed.mapAssetPriceFeed"],
-        //     shared: async () => {
-        //         return (await api.query.system.account(config.alice.address))
-        //             .data.free;
-        //     },
-        //     signed: config.alice,
-        //     pallet: "assetIndex",
-        //     call: "deposit",
-        //     args: [ASSET_ID_A, PINT.mul(ASSET_ID_A_DEPOSIT)],
-        //     verify: async (before: Balance) => {
-        //         const current = (
-        //             await api.query.system.account(config.alice.address)
-        //         ).data.free;
-        //
-        //         // cover weight fee
-        //         assert(
-        //             current.sub(before).div(PINT).toNumber() ===
-        //                 ASSET_ID_A_DEPOSIT.toNumber() - 1,
-        //             "assetIndex.deposit failed"
-        //         );
-        //     },
-        // },
-        // {
-        //     required: ["priceFeed.mapAssetPriceFeed"],
-        //     signed: config.alice,
-        //     pallet: "assetIndex",
-        //     call: "withdraw",
-        //     args: [PINT.mul(BALANCE_THOUSAND).div(new BN(4))],
-        //     verify: async () => {
-        //         assert(
-        //             (
-        //                 (
-        //                     await api.query.assetIndex.pendingWithdrawals(
-        //                         config.alice.address
-        //                     )
-        //                 ).toHuman() as any
-        //             ).length === 1,
-        //             "assetIndex.withdraw failed"
-        //         );
-        //     },
-        // },
-        // {
-        //     required: ["assetIndex.withdraw"],
-        //     shared: async () => {
-        //         const currentBlock = (
-        //             await api.derive.chain.bestNumber()
-        //         ).toNumber();
-        //         const pendingWithdrawls =
-        //             await api.query.assetIndex.pendingWithdrawals(
-        //                 config.alice.address
-        //             );
-        //
-        //         const end = (pendingWithdrawls as any).toHuman()[0].end_block;
-        //         const needsToWait =
-        //             end - currentBlock > WITHDRAWALS_PERIOD
-        //                 ? end - currentBlock - WITHDRAWALS_PERIOD
-        //                 : 0;
-        //
-        //         console.log(
-        //             `\t | waiting for the withdrawls peirod (around ${Math.floor(
-        //                 (needsToWait * 12) / 60
-        //             )} mins)...`
-        //         );
-        //
-        //         await waitBlock(needsToWait);
-        //     },
-        //     signed: config.alice,
-        //     pallet: "assetIndex",
-        //     call: "completeWithdraw",
-        //     args: [],
-        //     verify: async () => {
-        //         assert(
-        //             (
-        //                 (await api.query.assetIndex.pendingWithdrawals(
-        //                     config.alice.address
-        //                 )) as any
-        //             ).isNone,
-        //             "assetIndex.completeWithdraw failed"
-        //         );
-        //     },
-        // },
+        {
+            required: ["priceFeed.mapAssetPriceFeed"],
+            shared: async () => {
+                return (await api.query.system.account(config.alice.address))
+                    .data.free;
+            },
+            signed: config.alice,
+            pallet: "assetIndex",
+            call: "deposit",
+            args: [ASSET_ID_A, PINT.mul(ASSET_ID_A_DEPOSIT)],
+            verify: async (before: Balance) => {
+                const current = (
+                    await api.query.system.account(config.alice.address)
+                ).data.free;
+
+                // cover weight fee
+                assert(
+                    current.sub(before).div(PINT).toNumber() ===
+                        ASSET_ID_A_DEPOSIT.toNumber() - 1,
+                    "assetIndex.deposit failed"
+                );
+            },
+        },
+        {
+            required: ["priceFeed.mapAssetPriceFeed"],
+            signed: config.alice,
+            pallet: "assetIndex",
+            call: "withdraw",
+            args: [PINT.mul(BALANCE_THOUSAND).div(new BN(4))],
+            verify: async () => {
+                assert(
+                    (
+                        (
+                            await api.query.assetIndex.pendingWithdrawals(
+                                config.alice.address
+                            )
+                        ).toHuman() as any
+                    ).length === 1,
+                    "assetIndex.withdraw failed"
+                );
+            },
+        },
+        {
+            required: ["assetIndex.withdraw"],
+            shared: async () => {
+                const currentBlock = (
+                    await api.derive.chain.bestNumber()
+                ).toNumber();
+                const pendingWithdrawls =
+                    await api.query.assetIndex.pendingWithdrawals(
+                        config.alice.address
+                    );
+
+                const end = (pendingWithdrawls as any).toHuman()[0].end_block;
+                const needsToWait =
+                    end - currentBlock > WITHDRAWALS_PERIOD
+                        ? end - currentBlock - WITHDRAWALS_PERIOD
+                        : 0;
+
+                console.log(
+                    `\t | waiting for the withdrawls peirod (around ${Math.floor(
+                        (needsToWait * 12) / 60
+                    )} mins)...`
+                );
+
+                await waitBlock(needsToWait);
+            },
+            signed: config.alice,
+            pallet: "assetIndex",
+            call: "completeWithdraw",
+            args: [],
+            verify: async () => {
+                assert(
+                    (
+                        (await api.query.assetIndex.pendingWithdrawals(
+                            config.alice.address
+                        )) as any
+                    ).isNone,
+                    "assetIndex.completeWithdraw failed"
+                );
+            },
+        },
         /* remote-asset-manager*/
         {
             required: ["priceFeed.mapAssetPriceFeed"],
@@ -232,140 +232,140 @@ const TESTS = (api: ApiPromise, config: ExtrinsicConfig): Extrinsic[] => {
                 );
             },
         },
-        // /* committee */
-        // {
-        //     signed: config.alice,
-        //     pallet: "committee",
-        //     call: "propose",
-        //     args: [api.tx.balances.transfer(config.bob.address, 1000000)],
-        //     verify: async () => {
-        //         const proposals = await api.query.committee.activeProposals();
-        //         assert(
-        //             (proposals as any).length > 0,
-        //             "no proposal found after committe.propose"
-        //         );
-        //     },
-        // },
-        // {
-        //     required: ["committee.propose"],
-        //     shared: async () => {
-        //         return new Promise(async (resolve) => {
-        //             await waitBlock(1);
-        //             const hash = (
-        //                 (await api.query.committee.activeProposals()) as any
-        //             )[0];
-        //             const currentBlock = (
-        //                 await api.derive.chain.bestNumber()
-        //             ).toNumber();
-        //
-        //             const end = (
-        //                 (await api.query.committee.votes(hash)).toJSON() as any
-        //             ).end as number;
-        //
-        //             const needsToWait =
-        //                 end - currentBlock > VOTING_PERIOD
-        //                     ? end - currentBlock - VOTING_PERIOD
-        //                     : 0;
-        //
-        //             console.log(
-        //                 `\t | waiting for the voting peirod (around ${Math.floor(
-        //                     (needsToWait * 12) / 60
-        //                 )} mins)...`
-        //             );
-        //
-        //             await waitBlock(needsToWait);
-        //             resolve(hash);
-        //         });
-        //     },
-        //     signed: config.alice,
-        //     pallet: "committee",
-        //     call: "vote",
-        //     args: [(hash: string) => hash, api.createType("Vote" as any)],
-        //     with: [
-        //         async (hash: string): Promise<IExtrinsic> => {
-        //             return {
-        //                 signed: config.bob,
-        //                 pallet: "committee",
-        //                 call: "vote",
-        //                 args: [hash, api.createType("Vote" as any)],
-        //             };
-        //         },
-        //         async (hash: string): Promise<IExtrinsic> => {
-        //             return {
-        //                 signed: config.charlie,
-        //                 pallet: "committee",
-        //                 call: "vote",
-        //                 args: [hash, api.createType("Vote" as any)],
-        //             };
-        //         },
-        //         async (hash: string): Promise<IExtrinsic> => {
-        //             return {
-        //                 signed: config.dave,
-        //                 pallet: "committee",
-        //                 call: "vote",
-        //                 args: [hash, api.createType("Vote" as any)],
-        //             };
-        //         },
-        //     ],
-        //     verify: async (hash: string) => {
-        //         assert(
-        //             ((await api.query.committee.votes(hash)).toJSON() as any)
-        //                 .votes[0].vote === "Aye",
-        //             "committee.vote failed"
-        //         );
-        //     },
-        // },
-        // {
-        //     required: ["committee.vote"],
-        //     shared: async () => {
-        //         const hash = (
-        //             (await api.query.committee.activeProposals()) as any
-        //         )[0];
-        //         return hash;
-        //     },
-        //     signed: config.alice,
-        //     pallet: "committee",
-        //     call: "close",
-        //     args: [(hash: string) => hash],
-        //     verify: async (hash: string) => {
-        //         const proposals = await api.query.committee.executedProposals(
-        //             hash
-        //         );
-        //         assert(
-        //             (proposals as any).isSome,
-        //             "no proposal executed after committe.close"
-        //         );
-        //     },
-        // },
-        // {
-        //     pallet: "committee",
-        //     call: "addConstituent",
-        //     args: [config.ziggy.address],
-        //     verify: async () => {
-        //         assert(
-        //             (
-        //                 (await api.query.committee.members(
-        //                     config.ziggy.address
-        //                 )) as any
-        //             ).isSome,
-        //             "Add constituent failed"
-        //         );
-        //     },
-        // },
-        // /* local_treasury */
-        // {
-        //     pallet: "localTreasury",
-        //     call: "withdraw",
-        //     args: [500000000000, config.ziggy.address],
-        //     verify: async () => {
-        //         assert(
-        //             (
-        //                 await api.derive.balances.all(config.ziggy.address)
-        //             ).freeBalance.toNumber() === 500000000000,
-        //             "localTreasury.withdraw failed"
-        //         );
-        //     },
-        // },
+        /* committee */
+        {
+            signed: config.alice,
+            pallet: "committee",
+            call: "propose",
+            args: [api.tx.balances.transfer(config.bob.address, 1000000)],
+            verify: async () => {
+                const proposals = await api.query.committee.activeProposals();
+                assert(
+                    (proposals as any).length > 0,
+                    "no proposal found after committe.propose"
+                );
+            },
+        },
+        {
+            required: ["committee.propose"],
+            shared: async () => {
+                return new Promise(async (resolve) => {
+                    await waitBlock(1);
+                    const hash = (
+                        (await api.query.committee.activeProposals()) as any
+                    )[0];
+                    const currentBlock = (
+                        await api.derive.chain.bestNumber()
+                    ).toNumber();
+
+                    const end = (
+                        (await api.query.committee.votes(hash)).toJSON() as any
+                    ).end as number;
+
+                    const needsToWait =
+                        end - currentBlock > VOTING_PERIOD
+                            ? end - currentBlock - VOTING_PERIOD
+                            : 0;
+
+                    console.log(
+                        `\t | waiting for the voting peirod (around ${Math.floor(
+                            (needsToWait * 12) / 60
+                        )} mins)...`
+                    );
+
+                    await waitBlock(needsToWait);
+                    resolve(hash);
+                });
+            },
+            signed: config.alice,
+            pallet: "committee",
+            call: "vote",
+            args: [(hash: string) => hash, api.createType("Vote" as any)],
+            with: [
+                async (hash: string): Promise<IExtrinsic> => {
+                    return {
+                        signed: config.bob,
+                        pallet: "committee",
+                        call: "vote",
+                        args: [hash, api.createType("Vote" as any)],
+                    };
+                },
+                async (hash: string): Promise<IExtrinsic> => {
+                    return {
+                        signed: config.charlie,
+                        pallet: "committee",
+                        call: "vote",
+                        args: [hash, api.createType("Vote" as any)],
+                    };
+                },
+                async (hash: string): Promise<IExtrinsic> => {
+                    return {
+                        signed: config.dave,
+                        pallet: "committee",
+                        call: "vote",
+                        args: [hash, api.createType("Vote" as any)],
+                    };
+                },
+            ],
+            verify: async (hash: string) => {
+                assert(
+                    ((await api.query.committee.votes(hash)).toJSON() as any)
+                        .votes[0].vote === "Aye",
+                    "committee.vote failed"
+                );
+            },
+        },
+        {
+            required: ["committee.vote"],
+            shared: async () => {
+                const hash = (
+                    (await api.query.committee.activeProposals()) as any
+                )[0];
+                return hash;
+            },
+            signed: config.alice,
+            pallet: "committee",
+            call: "close",
+            args: [(hash: string) => hash],
+            verify: async (hash: string) => {
+                const proposals = await api.query.committee.executedProposals(
+                    hash
+                );
+                assert(
+                    (proposals as any).isSome,
+                    "no proposal executed after committe.close"
+                );
+            },
+        },
+        {
+            pallet: "committee",
+            call: "addConstituent",
+            args: [config.ziggy.address],
+            verify: async () => {
+                assert(
+                    (
+                        (await api.query.committee.members(
+                            config.ziggy.address
+                        )) as any
+                    ).isSome,
+                    "Add constituent failed"
+                );
+            },
+        },
+        /* local_treasury */
+        {
+            pallet: "localTreasury",
+            call: "withdraw",
+            args: [500000000000, config.ziggy.address],
+            verify: async () => {
+                assert(
+                    (
+                        await api.derive.balances.all(config.ziggy.address)
+                    ).freeBalance.toNumber() === 500000000000,
+                    "localTreasury.withdraw failed"
+                );
+            },
+        },
         /* chainlink_feed */
         {
             signed: config.alice,
@@ -420,19 +420,19 @@ const TESTS = (api: ApiPromise, config: ExtrinsicConfig): Extrinsic[] => {
                 );
             },
         },
-        // {
-        //     required: ["assetIndex.deposit"],
-        //     pallet: "priceFeed",
-        //     call: "unmapAssetPriceFeed",
-        //     args: [ASSET_ID_A],
-        //     verify: async () => {
-        //         assert(
-        //             ((await api.query.priceFeed.assetFeeds(ASSET_ID_A)) as any)
-        //                 .isNone,
-        //             "unmap price feed failed"
-        //         );
-        //     },
-        // },
+        {
+            required: ["assetIndex.deposit"],
+            pallet: "priceFeed",
+            call: "unmapAssetPriceFeed",
+            args: [ASSET_ID_A],
+            verify: async () => {
+                assert(
+                    ((await api.query.priceFeed.assetFeeds(ASSET_ID_A)) as any)
+                        .isNone,
+                    "unmap price feed failed"
+                );
+            },
+        },
         // /* saft-registry */
         // {
         //     signed: config.alice,
