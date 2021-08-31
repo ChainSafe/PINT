2e09a20c00152402a5372a57a4a220f2e73a9c51
Matthias Seitz <matthias.seitz@outlook.de>
2021-08-31 15:50:27 +0200

    fix: remove some AssetId bounds and add TryFrom<u8> (#308)
    * chore: apply orml patches
    
    * fix: remove restrictive trait bounds
    
    * feat: add TryFrom<u8> to assetid
    
    * fix: patch orml
    
    * fix: feature gate price feed benchmarks
    
    * rustfmt
    
    * fix: feature gate price feed
    
    * chore(price-feed): fix the imports of PriceFeedBenchmarks
    
    Co-authored-by: clearloop <udtrokia@gmail.com>

--- pallets/price-feed/src/benchmarking.rs
+++ pallets/price-feed/src/benchmarking.rs
@@ -5,14 +5,18 @@
 
 use super::*;
 use frame_benchmarking::benchmarks;
-use frame_support::{assert_ok, dispatch::UnfilteredDispatchable, sp_std::convert::TryInto, traits::EnsureOrigin};
+use frame_support::{
+	assert_ok,
+	dispatch::UnfilteredDispatchable,
+	sp_std::convert::{TryFrom, TryInto},
+	traits::EnsureOrigin,
+};
 
 use crate::Pallet as PriceFeed;
-use codec::{Decode, Encode};
 
 benchmarks! {
 	map_asset_price_feed {
-		let asset_id = T::AssetId::decode(&mut 2u64.encode().as_ref()).ok().unwrap();
+		let asset_id = T::AssetId::try_from(2u8).ok().unwrap();
 		let origin = T::AdminOrigin::successful_origin();
 		let feed_id = 0u32.try_into().ok().unwrap();
 		let call = Call::<T>::map_asset_price_feed(
@@ -27,7 +31,7 @@ benchmarks! {
 	}
 
 	unmap_asset_price_feed {
-		let asset_id = T::AssetId::decode(&mut 2u64.encode().as_ref()).ok().unwrap();
+		let asset_id = T::AssetId::try_from(2u8).ok().unwrap();
 		let origin = T::AdminOrigin::successful_origin();
 		let feed_id = 0u32.try_into().ok().unwrap();
 		assert_ok!(PriceFeed::<T>::map_asset_price_feed(origin.clone(), asset_id.clone(), feed_id));
