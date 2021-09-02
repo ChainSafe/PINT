63fcac73e639e15b74baddb7d96362fdd7d8b6b8
clearloop <udtrokia@gmail.com>
2021-09-02 13:09:52 +0800

    chore(common): add docs to origins in runtime-common

--- runtime/common/src/types.rs
+++ runtime/common/src/types.rs
@@ -1,12 +1,12 @@
 // Copyright 2021 ChainSafe Systems
 // SPDX-License-Identifier: LGPL-3.0-only
 
-/// Origin that approved by committee
+/// Origin either `Root` or `CommitteeOrigin`
 pub type GovernanceOrigin<AccountId, Runtime> = frame_system::EnsureOneOf<
 	AccountId,
 	pallet_committee::EnsureApprovedByCommittee<Runtime>,
 	frame_system::EnsureRoot<AccountId>,
 >;
 
-/// committee origin only
+/// Origin that approved by committee
 pub type CommitteeOrigin<Runtime> = pallet_committee::EnsureApprovedByCommittee<Runtime>;
