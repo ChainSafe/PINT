7e90178b995f91dfcbca7aaa8eee697dc974fba3
clearloop <udtrokia@gmail.com>
2021-09-03 18:33:21 +0800

    feat(runtime): add weights of local-treasury

--- pallets/local-treasury/src/benchmarking.rs
+++ pallets/local-treasury/src/benchmarking.rs
@@ -7,7 +7,7 @@ use frame_system::Origin;
 
 benchmarks! {
 	withdraw {
-		let local_treasury: <T as frame_system::Config>::AccountId = PalletId(*b"12345678").into_account();
+		let local_treasury: <T as frame_system::Config>::AccountId = PalletId(*b"Treasury").into_account();
 		T::Currency::deposit_creating(&local_treasury, 10_000_000_u32.into());
 		let admin: <T as frame_system::Config>::AccountId = account("admin", 0, 0);
 	}: _(
