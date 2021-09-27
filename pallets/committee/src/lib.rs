d7b5aa618a9173e2dce3ac829df53404471153fe
clearloop <mercuryfletcher@gmail.com>
2021-09-27 20:23:52 +0800

    make clippy happy

--- pallets/committee/src/lib.rs
+++ pallets/committee/src/lib.rs
@@ -494,9 +494,9 @@ pub mod pallet {
 				let ty = maybe_member.take().ok_or(Error::<T>::NotMember)?;
 
 				// Check if have enough council members
-				if ty == MemberType::Constituent
-					|| Members::<T>::iter_values().filter(|m| *m == MemberType::Council).count()
-						> T::MinCouncilVotes::get()
+				if ty == MemberType::Constituent ||
+					Members::<T>::iter_values().filter(|m| *m == MemberType::Council).count() >
+						T::MinCouncilVotes::get()
 				{
 					VotingEligibility::<T>::take(&member);
 					Ok(ty)
