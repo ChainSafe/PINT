7cd863c2ed39c9682c18714e2be716426815a4f8
clearloop <mercuryfletcher@gmail.com>
2021-10-29 14:58:47 +0800

    chore(asset-index): rustfmt

--- pallets/asset-index/src/lib.rs
+++ pallets/asset-index/src/lib.rs
@@ -711,8 +711,8 @@ pub mod pallet {
 					.into_iter()
 					.filter_map(|mut redemption| {
 						// only try to close if the lockup period is over
-						if redemption.end_block >= current_block
-							&& Self::do_complete_redemption(&caller, &mut redemption.assets)
+						if redemption.end_block >= current_block &&
+							Self::do_complete_redemption(&caller, &mut redemption.assets)
 						{
 							// all individual redemptions withdrawn, can remove them from storage
 							Self::deposit_event(Event::WithdrawalCompleted(caller.clone(), redemption.assets));
